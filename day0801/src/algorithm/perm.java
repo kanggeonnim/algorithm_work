28ae0e83a45e4bf76d226ae3156cced3d0ccf45b
kanggeonnim <gun4329@gmail.com>
2023-08-02 17:26:07 +0900

    init

---
+++ day0801/src/algorithm/perm.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class perm {
+	static int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	static boolean isSelected[] = new boolean[10];
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void perm(int cnt) {
+		if (cnt == 3) {
+			
+		}
+	}
+}
